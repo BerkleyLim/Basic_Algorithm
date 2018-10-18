d2b4d34267e413dbaad7442478a91b69284d909a
Your Name <you@example.com>
2018-10-18 19:24:10 +0900

    알고리즘 18.10.18

---
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/add/array/SumArrayAlgorithm.java
@@ -0,0 +1,34 @@
+package author.hyun.sik.lim.add.array;
+
+/**
+ * 
+ * <PRE>
+ * 1. ClassName : SumArrayAlgorithm
+ * 2. FileName  : SumArrayAlgorithm.java
+ * 3. Package  : author.hyun.sik.lim.add.array
+ * 4. Comment  : 다음은 배열의 합을 구하는 알고리즘이다
+ * 5. 작성자   : limhs
+ * 6. 작성일   : 2018. 10. 18. 오후 7:18:30
+ * </PRE>
+ */
+
+/**
+ * 
+ * 배열의 합 알고리즘
+ * - n개의 수로 된 배열 S에 있는 모든 수를 더하기!
+ * - 양의 정수 n, 배열 S(첨자는 1 ~ n)
+ * - S에 있는 수의 합, sum
+ */
+
+public class SumArrayAlgorithm {
+    int sum (int n, final int S[]) {
+        int i; // 인덱스
+        int result; // 결과 값 (number)
+        result = 0;
+        
+        for (i = 1; i<=n; i++) 
+            result += S[i];
+        
+        return result;
+    }
+}
---
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/search/seqeuntial/SequentialSearchAlgorithm.java
@@ -0,0 +1,38 @@
+package author.hyun.sik.lim.search.seqeuntial;
+
+/**
+ * 
+ * <PRE>
+ * 1. ClassName : SequentialSearchAlgorithm
+ * 2. FileName  : SequentialSearchAlgorithm.java
+ * 3. Package  : author.hyun.sik.lim.search.seqeuntial
+ * 4. Comment  :  다음은 순차 정렬의 알고리즘을 작성하였다!
+ * 5. 작성자   : limhs
+ * 6. 작성일   : 2018. 10. 18. 오후 7:07:16
+ * </PRE>
+ */
+
+/**
+ * 
+ * 순차 정렬 알고리즘
+ * - S : 배열에 저장된 값
+ * - n : 배열의 n개의 키
+ * - x : 배열의 키값
+ * - location : x의 위치
+ */
+
+// 입력 
+public class SequentialSearchAlgorithm {
+    // 순차정렬 알고리즘을 구성한다!
+    // 단 s[0] 자리를 사용하지 않고,
+    // 보통은 S[n+1] 형태로 사용
+    void seqsearch (int n, final Object s[], Object x, int location) {
+        location = 1;
+        
+        // 
+        while (location <= n && s[location] != x)
+            location++;
+        if (location > n)
+            location = 0;
+    }
+}
