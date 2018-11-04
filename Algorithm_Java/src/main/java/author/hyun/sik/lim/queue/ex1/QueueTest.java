06e1f180c8bfb6686230d3a89b393d225d313f58
berkleylim <berkleylim16@gmail.com>
2018-11-04 16:46:32 +0900

    Java로 만든 Queue의 관련 알고리즘

---
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/queue/ex1/QueueTest.java
@@ -0,0 +1,19 @@
+package author.hyun.sik.lim.queue.ex1;
+
+/*
+ * 다음은 큐의 대한 알고리즘의 대해 작성해보는 소스입니다.
+ * 
+ * 비록 Java API에 Queue라는 제네릭을 사용하면 되지만
+ * C언어 스타일로 알고리즘을 만들어 보고 나서 다시 적용합니다. 
+ * (API로 호출해서 사용하는 것보다 직접 자기자신이 API를 많이 만들면 실행시간을 줄일 수 있다.)
+ * 
+ * */
+
+public class QueueTest {
+    
+    
+    // main은 테스트 용도로 쓴다!!
+    public static void main(String[] args) {
+        
+    }
+}
