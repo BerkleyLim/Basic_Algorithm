919faea8a63d99c24795454cffcdd3146674ef71
berkleylim <berkleylim16@gmail.com>
2020-03-18 22:48:09 +0900

    Solution

--- Algorithm_Java/src/main/java/author/hyun/sik/lim/sw/expert/academy/Q1225/Solution.java
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/sw/expert/academy/Q1225/Solution.java
@@ -1,5 +1,116 @@
 package author.hyun.sik.lim.sw.expert.academy.Q1225;
 
-public class Solution {
+import java.util.Scanner;
 
+// 암호 생성기 문제 Q1225
+
+// n개의 수를 처리 시 8개 숫자 입력 받고,
+// 첫 번째 숫자를 1 감소 후, 맨 뒤로 보냄
+// 다음 첫 번째 수는 2 감소 후 맨 뒤로, 다음 첫번째 수 3 감소
+// 다음 첫번째 수 4 감소, 다음 첫번째 수 5 감소
+// 1 부터 5까지 감소를 하며 사이클 진행한다.
+// 이와 같이 반복 작업을 실행하여 사이클 진행
+// 숫자 감소 시 0보다 작아지는 경우 0으로 유지되며 프로그램 종료!
+// 마지막 암호 배열은 모두 한 자리 수로 구성!
+
+public class Solution {
+    
+    // 노드 객체 생성!
+    static class Node {
+        int data;
+        Node next;
+    }
+    
+    // 초기 값! (동적으로 사용 시 createLinkedQueue 메소드 혹은 생성자에 적용)
+    static Node front;
+    static Node rear;
+    
+    static final int SIZE = 8;
+    // 저장 변수
+    //static Node map;
+    
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = 0;
+        
+        // 프로그래밍 시작!
+        while(T < 10) {
+            front = null;
+            rear = null;
+            T = sc.nextInt();
+            //map = new Node();
+            
+            // 입력
+            for (int i = 0; i < SIZE; i++) {
+                enQueue(sc.nextInt());
+            }
+            
+            int i = 1;
+            // 알고리즘 실행
+            while (true) {
+                if (i == 6) 
+                    i = 1;
+                
+                int check = logic(i);
+                
+                if(check <= 0) {
+                    rear.data = 0;
+                    break;
+                }
+                
+                i++;
+            }
+            
+            System.out.print("#" + T + " ");
+            
+            for (int x = 0; x < SIZE; x++) {
+                System.out.print(deQueue() + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static int logic(int number) {
+        Node node = new Node();
+        node.data = front.data - number;
+        node.next = null;
+        
+        front = front.next;
+        rear.next = node;
+        rear = node;
+        
+        return node.data;
+    }
+    
+    public static void enQueue(int data) {
+        // 초기화
+        Node node = new Node();
+        node.data = data;
+        node.next = null;
+        
+        if (isEmpty()) {
+            //map = node;
+            front = node;
+            rear = node;
+        } else {
+            //map.next = node;
+            rear.next = node;
+            rear = node;
+        }
+    }
+    
+    // 삭제 연결 리스트 프로그래밍! (연습용)
+    public static int deQueue() {
+        if(isEmpty()) return -1;
+        else {
+            Node node = front;
+            front = front.next;
+            return node.data;
+        }
+    }
+    
+    // 연결 리스트! 비어있는지 검사
+    public static boolean isEmpty() {
+        return (front == null && rear == null);
+    }
 }
---
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/sw/expert/academy/Q1226/Solution.java
@@ -0,0 +1,97 @@
+package author.hyun.sik.lim.sw.expert.academy.Q1226;
+
+import java.util.Scanner;
+
+// 미로 찾기 문제!
+// BFS 문제로 해결!
+// map에서 수치 정보
+// 0 : 길 / 1 : 벽 / 2 : 출발점 / 3 : 도착점
+// 미로 찾기 가능 여부
+// 1 : 가능 , 0 : 불가능
+
+public class Solution {
+    static class NodeXY {
+        int level;
+        int x;
+        int y;
+        NodeXY link;
+    }
+    
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+    
+    static int[][] map;
+    static final int SIZE = 16;
+    static boolean[][] visited;
+    static NodeXY start;
+    static NodeXY end;
+    static NodeXY front;
+    static NodeXY rear;
+    
+    
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int testNumber = 0;
+        while(testNumber < 10) {
+            // 초기화
+            map = new int[SIZE][SIZE];
+            visited = new boolean[SIZE][SIZE];
+            
+            testNumber =sc.nextInt();
+            
+            // 출발점, 도착점 설정
+            start = new NodeXY();
+            end = new NodeXY();
+            
+            // setting
+            for (int y = 0; y < SIZE; y++) {
+                for (int x = 0; x < SIZE; x++) {
+                    map[y][x] = sc.nextInt();
+                    
+                    if (map[y][x] == 2) {
+                        start.x = x;
+                        start.y = y;
+                        // 시작점 지정
+                        enQueue(0,x,y);
+                    } else if (map[y][x] == 3) {
+                        end.x = x;
+                        end.y = y;
+                    } else if (map[y][x] == 1) {
+                        visited[y][x] = true;
+                    } else {
+                        visited[y][x] = false;
+                    }
+                }
+            }
+            
+            int level = 1;
+            // 도착점 찾을때까지 찾기!
+            while(isEmpty()) {
+                
+                for(int i = 0; i < 4; i++) {
+                    
+                }
+                
+                level++;
+            }
+        }
+        
+    }
+    
+    public static void enQueue(int level, int x, int y) {
+        // 큐 생성 시 방문 완료
+        visited[y][x] = true;
+        
+        
+    }
+    
+    public static void deQueue() {
+        
+    }
+    
+    public static boolean isEmpty() {
+        return (front == null && rear == null);
+    }
+    
+}
