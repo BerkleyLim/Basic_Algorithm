d111202f318e1a2d8b9a23d05205668bded3d7b8
berkleylim <berkleylim16@gmail.com>
2020-03-19 19:16:59 +0900

    BFS 구현

--- Algorithm_Java/src/main/java/author/hyun/sik/lim/sw/expert/academy/Q1228/Solution.java
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/sw/expert/academy/Q1228/Solution.java
@@ -1,4 +1,4 @@
-package author.hyun.sik.lim.sw.expert.academy.Q1226;
+package author.hyun.sik.lim.sw.expert.academy.Q1228;
 
 import java.util.Scanner;
 
@@ -8,121 +8,114 @@ import java.util.Scanner;
 // 0 : 길 / 1 : 벽 / 2 : 출발점 / 3 : 도착점
 // 미로 찾기 가능 여부
 // 1 : 가능 , 0 : 불가능
+// 가로 * 세로 : 100 * 100
 
 public class Solution {
     static class NodeXY {
-        int level;
         int x;
         int y;
+        int level;
         NodeXY link;
     }
     
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
-    
-    //static int[][] map;
-    static final int SIZE = 16;
-    static boolean[][] visited;
-    static NodeXY start;
-    static NodeXY end;
     static NodeXY front;
     static NodeXY rear;
     
+    // BFS 사용시 필수!
+    static final int[] dx = {0,0,1,-1};
+    static final int[] dy = {-1,1,0,0};
+    
+    static boolean[][] visited;
+    
+    static int startX;
+    static int startY;
+    static int endX;
+    static int endY;
+    
+    static int SIZE = 100;
     
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         
         int testNumber = 0;
         while(testNumber < 10) {
-            // 초기화
-            //map = new int[SIZE][SIZE];
-            visited = new boolean[SIZE][SIZE];
             front = null;
             rear = null;
+            visited = new boolean[SIZE][SIZE];
             
             testNumber = sc.nextInt();
             sc.nextLine();
-            // 출발점, 도착점 설정
-            start = new NodeXY();
-            end = new NodeXY();
             
-            // setting
+            
             for (int y = 0; y < SIZE; y++) {
                 String str = sc.nextLine();
                 
                 for (int x = 0; x < SIZE; x++) {
-                    int[][] map = new int[SIZE][SIZE];
-                    map[y][x] = str.charAt(x);
-                    //System.out.print(map[y][x] + " ");
-                    
-                    if (map[y][x] == '2') {
-                        start.x = x;
-                        start.y = y;
-                        // 시작점 지정
-                        enQueue(0,x,y);
-                    } else if (map[y][x] == '3') {
-                        end.x = x;
-                        end.y = y;
-                    } else if (map[y][x] == '1') {
+                    if (str.charAt(x) == '1')
                         visited[y][x] = true;
+                    else if (str.charAt(x) == '2') {
+                        startX = x;
+                        startY = y;
+                        enQueue(0, x, y);
+                    } else if (str.charAt(x) == '3') {
+                        endX = x;
+                        endY = y;
                     } else {
                         visited[y][x] = false;
                     }
                 }
             }
             
-            int answer = bfs(start.x,start.y,end.x,end.y);
             
-            System.out.println("#" + testNumber + " " + answer);
+            System.out.println("#" + testNumber + " " + bfs());
         }
         
     }
     
-    public static int bfs(int startX, int startY, int endX, int endY) {
-        int level = 1;
+    public static int bfs() {
+        // 초기 값 설정
         int x = startX;
         int y = startY;
+        int level = 1;
         
-        // 도착점 찾을때까지 찾기!
         while(!isEmpty()) {
-            // 정점 도착시!
+            // 여기서 정점을 찾은 경우!
             if (x == endX && y == endY)
                 return 1;
             
-            // 상하좌우 노드 검사 후 노드 생성
-            for(int i = 0; i < 4; i++) {
-                enQueue(level,x + dx[i], y + dy[i]);
+            // 인접 경로 탐색 (노드 검사) - 경로 있을 시 Queue 삽입
+            for (int i = 0; i < 4; i++) {
+                enQueue(level, x + dx[i], y + dy[i]);
             }
             
+            // 탐색 완료 후 다음 경로 노드 검사
             NodeXY node = deQueue();
             
-            if(node == null)
+            if (node == null)
                 return 0;
+            
             
             x = node.x;
             y = node.y;
-            level++;
         }
         
         return 0;
-        
     }
     
+    
     public static void enQueue(int level, int x, int y) {
-        // 방문 완료나 벽인 경우 실행 금지!
+        // 방문 완료시 생성 x 혹은 벽일 시 생성 금지
         if (visited[y][x]) return;
         
-        // 큐 생성 시 방문 완료
         visited[y][x] = true;
         
         NodeXY node = new NodeXY();
-        
-        node.level = level;
         node.x = x;
         node.y = y;
+        node.level = level;
         node.link = null;
         
-        if (isEmpty()) {
+        if(isEmpty()) {
             front = node;
             rear = node;
         } else {
@@ -142,13 +135,5 @@ public class Solution {
     public static boolean isEmpty() {
         return (front == null && rear == null);
     }
-    
-//    public static NodeXY qPeek() {
-//        if (isEmpty()) return null;
-//        else {
-//            
-//            return front;
-//        }
-//    }
     
 }
