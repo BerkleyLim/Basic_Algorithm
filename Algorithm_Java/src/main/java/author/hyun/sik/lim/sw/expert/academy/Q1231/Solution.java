9ef5555971bb1df725b4d7a76e547a9094096d53
berkleylim <berkleylim16@gmail.com>
2020-04-10 20:34:27 +0900

    Algorithm Solution

--- Algorithm_Java/src/main/java/author/hyun/sik/lim/sw/expert/academy/Q1231/Solution.java
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/sw/expert/academy/Q1231/Solution.java
@@ -3,21 +3,49 @@ package author.hyun.sik.lim.sw.expert.academy.Q1231;
 import java.util.Scanner;
 
 class Solution {
+    
+    static String[] node;
+    static int size;
+    static String answer;
+    
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         int T = 10;
 
         for(int test_case = 1; test_case <= T; test_case++) {
+            answer = "";
+            size = sc.nextInt();
             
+            node = new String[size + 1];
+            sc.nextLine();
+            
+            for (int i = 1; i <= size; i++) {
+                String str = sc.nextLine();
+                node[i] = str.split(" ")[1];
+            }
+            
+            System.out.print("#" + test_case + " ");
+            inOrder(1);
+            System.out.println(answer);
         }
     }
     
+    
+    
     // 중위 순회 알고리즘
-    public static void inorder() {
-        if(true/*트리가 비어있지 않을 때*/) {
-            // 왼쪽 먼저 순회
-            // visit = true;
-            // 오른쪽 순회
+    public static void inOrder(int index) {
+        if(isValid(index)) {/*지정한 크기가 더 클때*/
+            inOrder(index * 2);  // 왼쪽 먼저 순회
+            //System.out.print(node[index]);// visit = true;
+            answer += node[index];
+            inOrder((index * 2) + 1);  // 오른쪽 순회
         }
     }
+    
+    public static boolean isValid (int index) {
+        if (index > size)
+            return false;
+        return true;
+    }
+
 }
---
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/sw/expert/academy/Q1233/Solution.java
@@ -0,0 +1,17 @@
+package author.hyun.sik.lim.sw.expert.academy.Q1233;
+
+import java.util.Scanner;
+
+// 계산이 가능한지의 대한 여부 확인 문제
+// Tree의 중위순회로 사칙연산 구하기
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++) {
+            
+        }
+    }
+}
