4831b7a8a1b9047436b281f7f935514c8581f490
berkleylim <berkleylim16@gmail.com>
2020-06-11 20:09:55 +0900

    practice algorithm

--- Algorithm_Java/src/main/java/author/hyun/sik/lim/kakao/career/years19/no7/Solution.java
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/kakao/career/years19/no7/Solution.java
@@ -1,5 +1,8 @@
 package author.hyun.sik.lim.kakao.career.years19.no7;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 // 블록 이동하기
 // 준비중 로봇 : 2 * 1 크기
 // 0, 1로 이루어진 N*N 크기의 지도에서 2 * 1 크기인 로봇을 움직여 (N,N) 위치까지 이동 가능하도록 
@@ -11,6 +14,8 @@ package author.hyun.sik.lim.kakao.career.years19.no7;
 // 로봇 어디 칸이든 축이 되어 회전 가능
 // 로봇이 한 칸 이동 or 90도 회전하는데 걸리는 시간 : 1초
 // 로봇 위치가 N,N 위치까지 이동하는데 필요한 최소 시간을 return하기
+
+// 참고 : https://leveloper.tistory.com/102
 
 public class Solution {
     public static void main(String[] args) {
@@ -43,11 +48,25 @@ public class Solution {
     // 2) 오른쪽으로 이동 : 0 -> 1
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
+    static Queue<Point> queue;
+    static class Point {
+        int[] x, y;
+        int second;
+        Point (int[] x, int[] y, int second) {
+            this.x = x;
+            this.y = y;
+            this.second = second;
+        }
+    }
+    
     public static int solution(int[][] board) {
         int answer = 0;
         // 지도 크기 설정
         final int N = board.length;
-        
+        queue = new LinkedList<>();
+        int[] x = {0, 1};
+        int[] y = {0, 0};
+        queue.add(new Point(x,y,0));
         // 초기 값
         // 상단
         // (x,y) = (0,0), (1,0)
@@ -57,7 +76,33 @@ public class Solution {
         // 합해서 구하기
         
         // 도착까지 최소 걸리는 시간을 구하기!
-        
+        answer = bfs(board);
         return answer;
+    }
+
+    private static int bfs(int[][] board) {
+        // TODO Auto-generated method stub
+        int second = 0;
+        while(!queue.isEmpty()) {
+            Point point = queue.remove();
+            int[] x = point.x;
+            int[] y = point.y;
+            
+            // 각 방향별 이동 및 회전!
+            for (int i = 0; i < 4; i++) {
+                switch(i) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                }
+            }
+            second++;
+        }
+        return second;
     }
 }
