9978185fde69a607db198896e8eb574defe90250
berkleylim <berkleylim16@gmail.com>
2020-06-18 15:17:48 +0900

    Basic Algorithm String

--- Algorithm_Java/src/main/java/author/hyun/sik/lim/sw/expert/academy/Q10033_not/Solution.java
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/sw/expert/academy/Q10033_not/Solution.java
@@ -19,7 +19,6 @@ import java.util.StringTokenizer;
 // 
 public class Solution {
     static int indexB;
-    
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -27,69 +26,46 @@ public class Solution {
 
         for(int test_case = 1; test_case <= T; test_case++) {
             String str = init(br);
-            System.out.println(solution(str));
+            System.out.println("#" + test_case + " " + solution(str));
         }
         br.close();
     }
 
     private static int solution(String str) {
         // TODO Auto-generated method stub
+        indexB = str.indexOf("B");
+        //System.out.println(indexB);
         if (indexB < 0)
             return 0;
         
         int count = 0;
+        int Bcount = 1;
         int N = str.length();
         String token = str.substring(indexB, N);
-        while(true) {
-            boolean isExit = false;
-            
-            boolean isSearchB = false;
-            int Bend = 0;
-            int Wend = 0;
-            int Wcount = 0; // W 갯수만큼 곱한다.
-            String temp = "";
-            for (int i = 0; i < token.length(); i++) {
-                // 이거는 bw 패턴에 대해서 풀기 (brute 관련 알고리즘)
-//                if (token[i].equals("B") && token[i+1].equals("W")) {
-//                    // break 멈추기
-//                    if (isExit) { 
-//                        isExit = false;
-//                    }
-//                    count++;
-//                    // 바꾸기
-//                    token[i] = "W"; token[i+1] = "B";
-//                    index = i;
-//                    break;
-//                } else {
-//                    continue;
-//                }
-                
-                // 여기서는 B가 몇개이고 W가 몇개이며 B와 W의 갯수 지정
-                // B 갯수만큼 카운터를 센다
-                
-                if (token.substring(i,i+1).equals("B")) {
-                    if (isSearchB) {
-                        count *= Wcount;
-                        break;
-                    }
-                    Bend++;
-                    Wend++;
-                    count++;
-                } else {
-                    Wend++;
-                    Wcount++;
-                    isSearchB = false;
-                }
+        
+        // 여기서는 B가 몇개이고 W가 몇개이며 B와 W의 갯수 지정
+        // B 갯수만큼 카운터를 센다
+        // 이유는 : WWBBBBWWWWWBWBW 라고 주어질 경우를 생각한다.
+        /*
+            wwbbbwbwwwwwwwbwb - 3
+            wwbbwbwbwwwwwwwbb - 6
+            wwbwbwbwbwwwwwwbb - 9
+            wwwbwbwbwbwwwwwbb - 13
+            wwwwbwbwbwbwwwwbb - 17
+            wwwwwbwbwbwbwwwbb - 21
+            wwwwwwbwbwbwbwwbb - 25
+            wwwwwwwbwbwbwbwbb - 29
+            wwwwwwwwbwbwbwbbb - 33
+            wwwwwwwwwbwbwbbbb - 36
+            wwwwwwwwwwbwbbbbb - 38
+            wwwwwwwwwwwbbbbbb - 39
+         * */
+        for (int i = 1; i < token.length(); i++) {
+            if (token.substring(i,i+1).equals("B")) {
+                Bcount++;
+            } else {
+                count += Bcount;
             }
-            // 문자열 바꾸기
-            temp += token.substring(0, Bend);
-            //System.out.println(temp);
-            if (Wend != token.length())
-                temp += token.substring(Wend, token.length());
-            token = temp;  
-            //System.out.println(temp);
-            if (!temp.contains("W")) isExit = true;
-            if (isExit) break;
         }
         return count;
     }
@@ -98,7 +74,6 @@ public class Solution {
         // TODO Auto-generated method stub
         String str = br.readLine();
         
-        indexB = str.indexOf("B");
         return str;
     }
 }
