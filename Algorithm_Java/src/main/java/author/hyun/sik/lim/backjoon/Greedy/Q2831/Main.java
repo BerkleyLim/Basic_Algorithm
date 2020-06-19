76c163db82ffcd2fc1bed85e444c680c75958896
berkleylim <berkleylim16@gmail.com>
2020-06-19 17:30:47 +0900

    Basic Algorithm - Greedy

---
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/backjoon/Greedy/Q2831/Main.java
@@ -0,0 +1,13 @@
+package author.hyun.sik.lim.backjoon.Greedy.Q2831;
+
+// Greedy Algorithm 유형
+// 
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
