ebf29ece25284957eaa352c2c43be1c1be8c4d13
berkleylim <berkleylim16@gmail.com>
2020-06-28 18:49:25 +0900

    Basic Algorithm Q4112

---
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/sw/expert/academy/Q4112_not/Solution.java
@@ -0,0 +1,12 @@
+package author.hyun.sik.lim.sw.expert.academy.Q4112_not;
+
+// 이상한 피라미드 탐험
+// 
+public class Solution {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
