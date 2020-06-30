cb0c04a4266449859e81bebe017945aaf611c241
berkleylim <berkleylim16@gmail.com>
2020-06-30 19:21:14 +0900

    Basic Algorithm - Trial

--- Algorithm_Java/src/main/java/author/hyun/sik/lim/sw/expert/academy/Q1256/Solution.java
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/sw/expert/academy/Q1256/Solution.java
@@ -1,11 +1,34 @@
 package author.hyun.sik.lim.sw.expert.academy.Q1256;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 // 트라이 문자
 // 트라이 참고 : https://4ngs.tistory.com/24
 // K번째 접미어 찾기 문제
 
 
 public class Solution {
+    public static void main(String[] args) throws Exception {
+        // TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int test = 1; test <= T; test++) {
+            int K = Integer.parseInt(br.readLine());
+            String str = br.readLine();
+            bw.write("#"+test+" "+solution(K, str)+"\n");
+        }
+        
+        br.close();
+        bw.close();
+    }
+    
     static int N;
     static class TrieNode {
         TrieNode[] child;
@@ -15,9 +38,13 @@ public class Solution {
             this.isTerminal = false;
         }
     }
-    public static void main(String[] args) {
+    
+    private static String solution(int K, String str) {
         // TODO Auto-generated method stub
-
+        N = str.length();
+        String[] s = new String[N];
+        
+        return null;
     }
 
 }
