1b96d2f9d6fdff3c693906a25b228523854df950
berkleylim <berkleylim16@gmail.com>
2020-08-17 19:14:00 +0900

    solution

---
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/Solution4.java
@@ -0,0 +1,21 @@
+package author.hyun.sik.lim;
+
+public class Solution4 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int[] A = new int[] {1,4,2};
+        int[] B = new int[] {5,4,4};
+        System.out.println(solution(A,B));
+        
+        A = new int[] {1,2};
+        B = new int[] {3,4};
+        System.out.println(solution(A,B));
+    }
+
+    
+    public static int solution(int []A, int []B){
+        int answer = Integer.MAX_VALUE;
+        
+        return answer;
+    }
+}
