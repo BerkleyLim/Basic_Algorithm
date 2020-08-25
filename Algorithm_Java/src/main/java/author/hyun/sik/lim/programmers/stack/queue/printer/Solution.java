7c62b4a4f51568da38658af25f3488c390095bd6
berkleylim <berkleylim16@gmail.com>
2020-08-25 10:56:29 +0900

    Basic Algorithm - Stack/Queue (프린터)

---
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/programmers/stack/queue/printer/Solution.java
@@ -0,0 +1,23 @@
+package author.hyun.sik.lim.programmers.stack.queue.printer;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+//    1. 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
+//    2. 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
+//    3. 그렇지 않으면 J를 인쇄합니다.
+    // 문서 : A, B, C, D
+    // 순서 : 2, 1, 3, 2
+    // 출력 : C, D, A, B
+    // 중요도 : 숫자가 클수록 중요
+    // location : 0이상(현재 대기목록에 있는 작업수 -1)이하의 값!
+    //    대기목록 가장 앞 : 0, 두번째 : 1
+    public static int solution(int[] priorities, int location) {
+        int answer = 0;
+        return answer;
+    }
+}
