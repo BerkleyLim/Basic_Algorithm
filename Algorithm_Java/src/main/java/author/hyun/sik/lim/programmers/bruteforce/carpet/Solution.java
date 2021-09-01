00b2881058ee9ab0912b1572ef0657b866c72ff1
berkleylim <berkleylim16@gmail.com>
2021-09-02 00:18:25 +0900

    brute-force solution

--- Algorithm_Java/src/main/java/author/hyun/sik/lim/programmers/bruteforce/carpet/Solution.java
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/programmers/bruteforce/carpet/Solution.java
@@ -15,8 +15,30 @@ public class Solution {
     // 카펫 길이 : 가로 >= 세로
     // 카펫 길이의 가로와 세로 길이를 구하라
     public static int[] solution(int brown, int yellow) {
-        int[] answer = {};
+        int[] answer = {0,0};
         
+        // 갈색 타일 갯수 = (가로 길이* 2) + (세로 길이* 2) -4
+        // 노란색 타일 갯수 = (가로 길이 * 세로 길이) - 갈색 타일 갯수
+        // 갈색 타일 갯수 + 노란식 타일 갯수 = 가로 * 세로
+        for (int length = 1; length <= 5000; length++) { // 가로 길이
+            for (int width = 1; width <= length; width++) { // 세로 길이
+                
+                // 조건을 아래와 같이 할 경우 갈색 타일 갯수와 노란색 타일 갯수의 조건에 모순 된다.
+//                if ((brown + yellow) == ((length * width))) {
+//                    answer[0] = length; 
+//                    answer[1] = width; 
+//                    return answer;
+//                }
+                // 따라서 갈색 타일 갯수와, 노란색 타일 갯수를 일일히 검사할 것
+                if (brown == (length* 2) + (width * 2) - 4) {
+                    if (yellow == (length * width) - brown) {
+                        answer[0] = length; 
+                        answer[1] = width; 
+                        return answer;
+                    }
+                }
+            }
+        }
         
         return answer;
     }
