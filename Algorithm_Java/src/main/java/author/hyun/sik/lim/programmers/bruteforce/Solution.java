bc760d4e4fc8b2e3ffd69c5aa571da45b82a8a87
berkleylim <berkleylim16@gmail.com>
2021-09-01 23:27:13 +0900

    algorithm test (brute-force)

---
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/programmers/bruteforce/Solution.java
@@ -0,0 +1,15 @@
+package author.hyun.sik.lim.programmers.bruteforce;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+    }
+    
+    // Bruteforce 알고리즘 메뉴얼
+    public Object solution() {
+        
+        
+        return null;
+    }
+    
+}
---
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/programmers/bruteforce/carpet/Solution.java
@@ -0,0 +1,24 @@
+package author.hyun.sik.lim.programmers.bruteforce.carpet;
+
+public class Solution {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        System.out.println(solution(10,2)); // [4,3]
+        System.out.println(solution(8,1));  // [3,3]
+        System.out.println(solution(24,24)); // [8,6]
+    }
+    
+    
+    // brown : 8 ~ 5,000
+    // yellow : 1 ~ 2,000,000
+    // 카펫 길이 : 가로 >= 세로
+    // 카펫 길이의 가로와 세로 길이를 구하라
+    public static int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        
+        
+        return answer;
+    }
+
+}
