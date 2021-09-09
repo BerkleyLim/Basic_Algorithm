b76f0c1cdaf0f29bf7435ab5149c4c05cab7d43f
berkleylim <berkleylim16@gmail.com>
2021-09-10 01:04:47 +0900

    kakao21 algorithm

--- Algorithm_Java/src/main/java/author/hyun/sik/lim/programmers/kakao21/q4/Dijkstra.java
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/programmers/kakao21/q4/Dijkstra.java
@@ -1,10 +1,48 @@
 package author.hyun.sik.lim.programmers.kakao21.q4;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Dijkstra {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+        
+        // 82
+        System.out.println(solution(6, 4, 6, 2, 
+                new int[][] {{4, 1, 10}, 
+                             {3, 5, 24}, 
+                             {5, 6, 2}, 
+                             {3, 1, 41},
+                             {5, 1, 24}, 
+                             {4, 6, 50},
+                             {2, 4, 66},
+                             {2, 3, 22},
+                             {1, 6, 25}}));
+        
+        // 14
+        System.out.println(solution(7, 3, 4, 1, 
+                new int[][] {{5, 7, 9},
+                             {4, 6, 4},
+                             {3, 6, 1}, 
+                             {3, 2, 3}, 
+                             {2, 1, 6}}));
+        
+        // 18
+        System.out.println(solution(6, 4, 5, 6, 
+                new int[][] {{2, 6, 6},
+                             {6, 3, 7},
+                             {4, 6, 7}, 
+                             {6, 5, 11}, 
+                             {2, 5, 12},
+                             {5, 3, 20},
+                             {2, 4, 8},
+                             {4, 3, 9}}));
+        
+        
     }
     
     // 합승 택시 요금
@@ -23,8 +61,94 @@ public class Dijkstra {
     // 합승 택시 요금
     // 택시비를 아낄려는 전략 사용
     // A,B 두사람이 택시를 탈 경우 집으로 도착할 수 있는 최소 비용
+    
+    // 다익스트라 모드
+    // 참조 : https://wellbell.tistory.com/101
+    
+    static class Edge implements Comparable<Edge> {
+        int index;
+        int cost;
+        Edge(int index, int cost) {
+            this.index = index;
+            this.cost = cost;
+        }
+        
+       @Override
+       public int compareTo(Edge e) {
+           return this.cost - e.cost;
+       }
+    }
+    
+    // 정점 : 200, 요금 : 100000 기준(경로로 생각한다)
+    static final int MAX = 200 * 100000 + 1;
+    static LinkedList<LinkedList<Edge>> graph;
+    
     public static int solution(int n, int s, int a, int b, int[][] fares) {
-        int answer = 0;
+        int answer = MAX;
+        
+        // 그래프 정의
+        graph = new LinkedList<>();
+        
+        // 노드 생성
+        for (int i = 0; i <= n; i++) {
+            graph.add(new LinkedList<>());
+        }
+        
+        // 각각 요금(거리) 설정 (최단경로, <문제에선 최저요금>)
+        for(int[] i : fares){
+            graph.get(i[0]).add(new Edge(i[1], i[2]));
+            graph.get(i[1]).add(new Edge(i[0], i[2]));
+        }
+        
+        // 대문자는 각 start 지점
+        int[] A = new int[n + 1];
+        int[] B = new int[n + 1];
+        int[] S = new int[n + 1];
+        
+        Arrays.fill(A, MAX);
+        Arrays.fill(B, MAX);
+        Arrays.fill(S, MAX);
+        
+        // 각각 스타트 위치 지정하여 도착점까지
+        A = dijkstra(a, A);
+        B = dijkstra(b, B);
+        S = dijkstra(s, S);       
+        
+        for(int i = 1; i <= n ; i ++) 
+            answer = Math.min(answer, A[i] + B[i] + S[i]);
         return answer;
+    }
+    
+    // bfs 기반으로 다익스트라 알고리즘 사용한다
+    private static int[] dijkstra(int start, int[] costs) {
+        Queue<Edge> queue = new PriorityQueue<>();
+        
+        // queue를 이용
+        queue.offer(new Edge(start, 0));
+        costs[start] = 0;
+        
+        // bfs 시작
+        while (!queue.isEmpty()) {
+            Edge current = queue.poll();
+            int nodeIndex = current.index;
+            int nodeCost = current.cost;
+            
+            if (nodeCost > costs[nodeIndex])
+                continue;
+            
+            List<Edge> edges = graph.get(nodeIndex);
+            
+            for (Edge edge : edges) {
+                int cost = costs[nodeIndex] + edge.cost;
+                
+                if (cost < costs[edge.index]) {
+                    costs[edge.index] = cost;
+                    queue.offer(new Edge(edge.index, cost));
+                }
+            }
+            
+        }
+        
+        return costs;
     }
 }
--- Algorithm_Java/src/main/java/author/hyun/sik/lim/programmers/kakao21/q4/Floid.java
+++ Algorithm_Java/src/main/java/author/hyun/sik/lim/programmers/kakao21/q4/Floid.java
@@ -1,10 +1,44 @@
 package author.hyun.sik.lim.programmers.kakao21.q4;
 
+import java.util.Arrays;
+
 public class Floid {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+        
+        // 82
+        System.out.println(solution(6, 4, 6, 2, 
+                new int[][] {{4, 1, 10}, 
+                             {3, 5, 24}, 
+                             {5, 6, 2}, 
+                             {3, 1, 41},
+                             {5, 1, 24}, 
+                             {4, 6, 50},
+                             {2, 4, 66},
+                             {2, 3, 22},
+                             {1, 6, 25}}));
+        
+        // 14
+        System.out.println(solution(7, 3, 4, 1, 
+                new int[][] {{5, 7, 9},
+                             {4, 6, 4},
+                             {3, 6, 1}, 
+                             {3, 2, 3}, 
+                             {2, 1, 6}}));
+        
+        // 18
+        System.out.println(solution(6, 4, 5, 6, 
+                new int[][] {{2, 6, 6},
+                             {6, 3, 7},
+                             {4, 6, 7}, 
+                             {6, 5, 11}, 
+                             {2, 5, 12},
+                             {5, 3, 20},
+                             {2, 4, 8},
+                             {4, 3, 9}}));
+        
+        
     }
     
     // 합승 택시 요금
@@ -23,8 +57,56 @@ public class Floid {
     // 합승 택시 요금
     // 택시비를 아낄려는 전략 사용
     // A,B 두사람이 택시를 탈 경우 집으로 도착할 수 있는 최소 비용
+    
+    // 플로이드 모드
+    // 참조 1 : https://wellbell.tistory.com/101
+    // 참조 2 : https://blog.naver.com/ndb796/221234427842
+    
+    // 정점 : 200, 요금 : 100000 기준(경로로 생각한다)
+    static final int MAX = 200 * 100000 + 1;
+    
     public static int solution(int n, int s, int a, int b, int[][] fares) {
-        int answer = 0;
+        // 결과 그래프 초기화 1
+        int[][] dp = new int[n+1][n+1];
+        
+        // 결과 그래프 초기화 2
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], MAX);
+            dp[i][i] = 0;
+        }
+
+        // 정점 및 간선 입력
+        for (int i = 0; i < fares.length; i++) {
+            int start = fares[i][0];
+            int end = fares[i][1];
+            int cost = fares[i][2];
+
+            dp[start][end] = cost;
+            dp[end][start] = cost;
+        }
+
+        // k = 거쳐가는 노드
+        for (int k = 1; k < n+1; k++) {
+            // i = 출발 노드
+            for (int i = 1; i < n+1; i++) {
+                // j = 도착 노드
+                for (int j = 1; j < n+1; j++) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
+                }
+            }
+        }
+        
+        // 여기까지 플로이드 와샬 알고리즘 기본 형태
+        // ------------------------------------------------------------
+        
+        // 여기서부터 중간지 경유 시작
+        // 1) 각자 따로 도착지 계산
+        int answer = dp[s][a] + dp[s][b];
+        
+        // 2) 경유지점 끼고 갈 경우
+        for (int i = 1; i <= n; i++) {
+            answer = Math.min(answer, dp[s][i] + dp[i][a] +dp[i][b]);
+        }
         return answer;
     }
 }
