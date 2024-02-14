4fba23a735049ed90e0e918e0e3f382c4a7d931a
berkleylim <berkleylim16@gmail.com>
2024-02-14 23:08:57 +0900

    comment: descript running python - 파이썬이 어떻게 돌아가는지 코멘트 남기기

--- Algorithm_python/ex1.py
+++ Algorithm_python/ex1.py
@@ -1,9 +1,13 @@
 # 문제 : xyz321 입력 시 실행 결고
 
-x = input('입력 : ')
-a = ['abg123', 'def456', 'ghi789']
-a.append(x)
-a.remove('def456')
-print(a[1][-3:], a[2][:-3], sep = ',')
+x = input('입력 : ')        # console에서 입력 : xyz321
+a = ['abg123', 'def456', 'ghi789']  # 0 : abg123, 1 : def456, 2:ghi789
+a.append(x)                         # 0 : abg123, 1 : def456, 2:ghi789, 3 : xyz321
+a.remove('def456')                  # 0 : abg123, 1 : ghi789, 2 : xyz321
+print(a[1][-3:], a[2][:-3], sep = ',')      # 789, xyz
 for i in range(3,6) :
-  print(i, end = ' ')
+  print(i, end = ' ')               # 3 4 5
+  
+# 정답 
+# 789,xyz
+# 3 4 5
